fd0b629f8ebaf05358c786f003ee7b2abe284def
MinionAttack <10078549+MinionAttack@users.noreply.github.com>
2025-03-04 19:21:00 +0100

    Google Maps marker migration
    Changed the deprecated google.maps.Marker to the new google.maps.marker.AdvancedMarkerElement.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,17 @@
 // Google Maps
 function showMap() {
-    let map = new google.maps.Map(document.getElementById('map-container'), {zoom: 13});
+    let map = new google.maps.Map(document.getElementById('map-container'), {
+        zoom: 13,
+        mapId: "Online_CV_Contact",
+    });
     let geocoder = new google.maps.Geocoder;
     geocoder.geocode({'address': 'La Coruña'}, function (results, status) {
         if (status === 'OK') {
             map.setCenter(results[0].geometry.location);
-            new google.maps.Marker({
+            new google.maps.marker.AdvancedMarkerElement({
                 map: map,
-                position: results[0].geometry.location
+                position: results[0].geometry.location,
+                title: 'A Coruña',
             });
         } else {
             window.alert('Geocode was not successful for the following reason: ' + status);
